5009cff1e3be411f5da3b05a8e105b4ceaa00008
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-21 17:44:28 +0200

    completed app info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+
+use clap::{Arg, App};
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("smbup")
+        .version("0.0.1")
+        .author("Krzysztof Kulka <krzysztof.kulka1234@gmail.com>")
+        .about("samba cli tool for simple archivizng to local samaba server")
+        .arg(Arg::with_name("IP")
+                 .required(true)
+                 .takes_value(true)
+                 .index(1)
+                 .help("IP address of the samba server"))
+        .get_matches();
+    let url = matches.value_of("IP").unwrap();
+    println!("{}", url);
 }
