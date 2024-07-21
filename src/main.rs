95e57475628b66ad8e7c2e43bc0144982d143dd7
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-21 17:46:54 +0200

    progress bar function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate clap;
-
+extern crate indicatif;
+use indicatif::{ProgressBar, ProgressStyle};
 use clap::{Arg, App};
 fn main() {
     let matches = App::new("smbup")
@@ -14,4 +15,26 @@ fn main() {
         .get_matches();
     let url = matches.value_of("IP").unwrap();
     println!("{}", url);
+}
+fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = match quiet_mode {
+        true => ProgressBar::hidden(),
+        false => {
+            match length {
+                Some(len) => ProgressBar::new(len),
+                None => ProgressBar::new_spinner(),
+            }
+        }
+    };
+
+    bar.set_message(msg);
+    match length.is_some() {
+        true => bar
+            .set_style(ProgressStyle::default_bar()
+                .template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+                .progress_chars("=> ")),
+        false => bar.set_style(ProgressStyle::default_spinner()),
+    };
+
+    bar
 }
